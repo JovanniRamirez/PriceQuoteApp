45bba546e5ea074496eec958603830d85b930a4a
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-09-03 01:51:04 -0700

    Create Price Quote app and set fields

---
+++ PriceQuoteApp/Models/PriceQuotation.cs
@@ -0,0 +1,23 @@
+﻿// Ignore Spelling: App
+
+namespace PriceQuoteApp.Models
+{
+    public class PriceQuotation
+    {
+        public decimal Subtotal { get; set; }
+
+        public decimal DiscountPercent { get; set; }
+
+        public decimal DiscountAmount { get; set; }
+
+        public decimal Total { get; set; }
+
+        public PriceQuotation()
+        {
+            Subtotal = 0;
+            DiscountPercent = 0;
+            DiscountAmount = 0;
+            Total = 0;
+        }
+    }
+}
